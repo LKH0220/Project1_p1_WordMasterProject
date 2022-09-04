163911bae90f8a0091d064564a0df274ccf8ef8d
LKH0220 <22100482@handong.ac.kr>
2022-09-04 19:44:51 +0900

    First Commit

---
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -0,0 +1,64 @@
+package com.mycom.word;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordCRUD implements ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+
+    /*
+    * => 난이도(1,2,3) & 새 단어 입력 : 1 driveaway
+    * 뜻 입력 : 차고진입로
+    * 새 단어가 단어장에 추가되었습니다.
+    */
+
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
+
+    @Override
+    public Object add() {
+        //데이터를 입력받음
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        return new Word(0, level, word, meaning);
+    }
+
+    public void addWord(){
+        //add()로 입력받은 데이터를 list에 추가
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다.");
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
+
+    public void listAll(){
+        System.out.println("-----------------------------------------");
+        for(int i = 0; i<list.size(); i++){
+            System.out.print((i+1) + " ");
+            System.out.println(list.get(i).toString());
+        }
+        System.out.println("-----------------------------------------");
+    }
+}
