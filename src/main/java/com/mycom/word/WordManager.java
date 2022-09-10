2ab8d953931ad8a95863213ca13a67d287af428d
LKH0220 <22100482@handong.ac.kr>
2022-09-10 21:45:10 +0900

    Add Update & Delete function

--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -10,18 +10,18 @@ public class WordManager {
         wordCRUD = new WordCRUD(s);
     }
     public int selectMenu(){
-        System.out.println("*** 영단어 마스터 ***\n" +
-                "    ********************\n" +
-                "    1. 모든 단어 보기\n" +
-                "    2. 수준별 단어 보기\n" +
-                "    3. 단어 검색\n" +
-                "    4. 단어 추가\n" +
-                "    5. 단어 수정\n" +
-                "    6. 단어 삭제\n" +
-                "    7. 파일 저장\n" +
-                "    0. 나가기\n" +
-                "    ********************\n" +
-                "    => 원하는 메뉴는? ");
+        System.out.print("*** 영단어 마스터 ***\n" +
+                "********************\n" +
+                "1. 모든 단어 보기\n" +
+                "2. 수준별 단어 보기\n" +
+                "3. 단어 검색\n" +
+                "4. 단어 추가\n" +
+                "5. 단어 수정\n" +
+                "6. 단어 삭제\n" +
+                "7. 파일 저장\n" +
+                "0. 나가기\n" +
+                "********************\n" +
+                "=> 원하는 메뉴는? ");
         return s.nextInt();
     }
     public void start(){
@@ -32,10 +32,16 @@ public class WordManager {
                 break;
             }
             if(menu == 4) {
-                wordCRUD.addWord();
+                wordCRUD.addItem();
             }
             if(menu == 1){
                 wordCRUD.listAll();
+            }
+            if(menu == 5){  //update
+                wordCRUD.updateItem();
+            }
+            if(menu == 6){  //delete
+                wordCRUD.deleteItem();
             }
         }
     }
